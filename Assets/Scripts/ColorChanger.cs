f029a9b46a50ed423e1c0427116a0531811ae2e1
TheManfredCode <41202546+TheManfredCode@users.noreply.github.com>
2021-04-01 15:26:30 +0300

    ColorChanger added RequiredComponent

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class ColorChanger : MonoBehaviour
 {
     [SerializeField] private Color _targetColor;
